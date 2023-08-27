71fb0c822f02df3a68c8d52c0130cb78ee4ca5ff
Amro Diab <adiab@hotmail.co.uk>
2023-08-27 15:06:46 +0100

    full example

--- actions/hello_st2.py
+++ actions/hello_st2.py
@@ -1,8 +1,7 @@
-import os
 from st2common.runners.base_action import Action
+
 
 class HelloStackStorm(Action):
     def run(self):
-        os.system("bash test_api_call.sh")
         print("Hello Stackstorm, want to be friends?")
         return True, "Let's be friends!"
